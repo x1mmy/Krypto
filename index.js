034038016cef77aa6d06cfbb6ae1da93da165583
x1mmy <zimraan2012@gmail.com>
2021-11-20 13:18:32 +1100

    commit to 'Main'

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 //API KEY = coinrankingb2c724462ca9093fb0b7f399aa6b4eb95e6ee8774e20011b
 
+var baseUrl = "https://api.coinranking.com/v2/coins";
+var proxyUrl = "https://cors-anywhere.herokuapp.com/";
+var apiKey = "coinrankingb2c724462ca9093fb0b7f399aa6b4eb95e6ee8774e20011b";
 
+fetch (`${proxyUrl}${baseUrl}`, {
+    method: 'GET',
+    headers: {
+        'Content-type' : 'application/json',
+        'x-access-token' : `${apiKey}`,
+        'Access-Control-Allow-Origin': '*'
+    }
+}).then((response) => {
+    if(response.ok) {
+        response.json().then((json) => {
+            console.log(json.data.coins)
+
+            let coinsData = json.data.coins
+
+            if(coinsData.length > 0) {
+                var cryptoCoins = ""
+            }
+
+            //For Loop Starts
+            coinsData.forEach((coins) => {
+                cryptoCoins += "<tr>"
+                cryptoCoins += `<td> ${coins.btcPrice} </td>`;
+                cryptoCoins += `<td> ${coins.rank} </td>`;
+                cryptoCoins += `<td> ${coins.tier} </td>`;
+                cryptoCoins += `<td> ${coins.name} </td>`;
+                cryptoCoins += `<td>$${Math.round(coins.price)} </td>`;
+                cryptoCoins += `<td> ${coins.symbol} </td>`; "<tr>"
+            })
+            document.getElementById("data").innerHTML = cryptoCoins
+        })
+    }
+}).catch((error) => {
+    console.log(console.error)
+})
+
+
+
+//Interactive Shapes
 const scaleFactor = 1/20;
 
 function moveBackground(event){
